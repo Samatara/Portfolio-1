0f460a26e6c9f30a8c29ec120156f39c7c2a422f
Samatara <samatarbulbula@gmail.com>
2024-08-29 06:42:48 +0200

    js
    satt på slikk når man fyller in i feltene så skal prosjekt listen oppdateres og legger det nye prosjekte i en liste

--- my-app/my-app/src/main.js
+++ my-app/my-app/src/main.js
@@ -1,10 +1,36 @@
-function handlesubmit(event){
-    event.preventDefault;
-}
+document.addEventListener('DOMContentLoaded', ()=>{
+    const pro_list = document.getElementById('pro_list')
+    const form = document.getElementById('New-project')
+    
+    fetch('portofolio.Json')
+        .then (Response=> Response.json())
+        .then (jsdata => {
+            jsdata.projects.forEach(project => {
+               pro_list.innerHTML= `<h2>${project.name}</h2>
+                <p>${project.description}</p>`;
+                pro_list.appendChild(list)
+                
+            });
+        })
 
-const data= new FormData(Event.target)
 
-const value= object.fromEntries(data.entries)
+    form.addEventListener('submit',(event)=>{
+        event.preventDefault();
+        const projectName = document.getElementById('name').value;
+        const projectDescription = document.getElementById('description').value;
+        
+        const new_projects={
+            name:projectName,
+            description: projectDescription
+        };
 
+        const list = document.createElement('ul');
+        list.innerHTML= 
+        `<li>${new_projects.name}</li>
+        <li>${new_projects.description}</li>`;
+        pro_list.appendChild(list);
+        form.reset();
+    })
+})
 
 
